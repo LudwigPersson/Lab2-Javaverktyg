f1489f86aad05c62ec16ca5f727c152e90c04083
LudwigPersson <ludwig.persson@iths.se>
2024-01-30 18:34:20 +0100

    Empty string return 0 test

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -1,4 +1,9 @@
 package com.example;
 
 public class StringCalculator {
+
+    public static int add(String numbers) {
+        return 0;
+
+    }
 }
--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+ import static org.assertj.core.api.Assertions.*;
 
 class StringCalculatorTest {
+
+
+    @Test
+    @DisplayName("Add method should return 0 for an empty string")
+    void addMethodShouldReturn0ForAnEmptyString(){
+        int result = StringCalculator.add("");
+        assertEquals(0,result);
+
+    }
 
 }
